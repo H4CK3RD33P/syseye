687e3a95e5625557d389ddc375dd8cca3c385c66
Subhodeep Sarkar <subhodeep.sofficial@gmail.com>
2021-09-21 16:22:35 +0530

    Added base functions for monitoring system

--- main.py
+++ main.py
@@ -30,3 +30,40 @@ def create_and_send_email(recipient,subject,content,attachment=None):
 
     mail_server.send_message(email)
     mail_server.quit()
+
+cpuy = []
+tempy = []
+virty = []
+
+def cpu_usage(i):
+    percent = cpu_percent()
+    cpuy.append(percent)
+    plt.plot(cpuy, 'r', label='hello')
+    ax = plt.gca()
+    ax.set_xticklabels([])
+    plt.tight_layout()
+
+red = FuncAnimation( plt.gcf(), cpu_usage, interval = 100)
+
+def cpu_temperature(i):
+    percent = sensors_temperatures()['coretemp'][1][1]
+    tempy.append(percent)
+    plt.plot(tempy, 'b', label='hello')
+    ax = plt.gca()
+    ax.set_xticklabels([])
+    plt.tight_layout()
+
+blue = FuncAnimation( plt.gcf(), cpu_temperature, interval = 100)
+
+def ram_usage(i):
+    percent = list(virtual_memory())[2]
+    virty.append(percent)
+    plt.plot(virty, 'y', label='hello')
+    ax = plt.gca()
+    ax.set_xticklabels([])
+    plt.tight_layout()
+
+yellow = FuncAnimation( plt.gcf(), ram_usage, interval = 100)
+
+plt.title("BLUE -> CPU Temperature \n RED -> CPU Usage \n YELLOW ->  RAM Usage")
+plt.show()
